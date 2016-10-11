c73621a372f7ae5423262db57198435780aa303f
Francisco Souza <f@souza.cc>
2016-10-10 23:29:56 -0400

    Generate 1.1.1 and mark it as latest/stable

--- make.bash
+++ make.bash
@@ -55,8 +55,9 @@ clean () {
 install
 
 generate master master
-generate latest 1.1.0
-generate stable 1.1.0
+generate latest 1.1.1
+generate stable 1.1.1
+generate 1.1.1 1.1.1
 generate 1.1.0 1.1.0
 generate 1.0.1 1.0.1
 generate 1.0.0 1.0.0
