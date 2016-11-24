de05ae5ea1ace05707cd6e3901bc0f8404a9aa75
Cezar Sa Espinola <cezarsa@gmail.com>
2016-11-24 14:52:35 -0200

    add 1.2.0 rc release

--- make.bash
+++ make.bash
@@ -55,8 +55,9 @@ clean () {
 install
 
 generate master master
-generate latest 1.1.1
+generate latest 1.2.0-rc1
 generate stable 1.1.1
+generate 1.2.0 1.2.0-rc1
 generate 1.1.1 1.1.1
 generate 1.1.0 1.1.0
 generate 1.0.1 1.0.1
